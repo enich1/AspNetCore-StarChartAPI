630839940560382b7a737cfd810fa45cd49180fa
Eric Nicholson <eric-n@EMCINS.COM>
2020-09-19 06:59:05 -0500

    5.3 fix typo

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,7 +18,7 @@ namespace StarChart.Controllers
             _context = context;
         }
         [HttpGet("{id:int}",Name = "GetById")]
-        public IActionResult GetByID(int id)
+        public IActionResult GetById(int id)
         {
             var celestialObject = _context.CelestialObjects.Find(id);
             if (celestialObject == null)
